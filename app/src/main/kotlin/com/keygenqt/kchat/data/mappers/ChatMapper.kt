78fbe1b140b9965981626a13f6922abe38e84554
Vitaliy Zarubin <keygenqt@gmail.com>
2021-08-28 11:22:38 +0300

    Added UI view chat

--- app/src/main/kotlin/com/keygenqt/kchat/data/mappers/ChatMapper.kt
+++ app/src/main/kotlin/com/keygenqt/kchat/data/mappers/ChatMapper.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 package com.keygenqt.kchat.data.mappers
 
 import com.keygenqt.kchat.data.models.ChatModel
--- app/src/main/kotlin/com/keygenqt/kchat/data/requests/ChatCreateRequest.kt
+++ app/src/main/kotlin/com/keygenqt/kchat/data/requests/ChatCreateRequest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 package com.keygenqt.kchat.data.requests
 
 import androidx.compose.runtime.Immutable
